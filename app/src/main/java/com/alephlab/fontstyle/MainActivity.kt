e4617d7d104b015c1b47c0a062146e11888f4df0
SalmanAshraf70 <salman.ashraf@aleph-labs.com>
2021-09-07 17:08:10 +0800

    - Added font - Added recycleview

--- app/src/main/java/com/alephlab/fontstyle/MainActivity.kt
+++ app/src/main/java/com/alephlab/fontstyle/MainActivity.kt
@@ -2,10 +2,50 @@ package com.alephlab.fontstyle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.alephlab.fontstyle.databinding.ActivityMainBinding
+import com.alephlab.fontstyle.model.TextItem
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val adapter = HomeAdapter()
+    private lateinit var listOfFontNames: ArrayList<TextItem>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        addValue()
+
+        val recyclerView: RecyclerView = binding.accountRecyclerView
+        // Prepare views
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        val dividerItemDecoration = DividerItemDecoration(
+            recyclerView.context,
+            layoutManager.orientation
+        )
+        recyclerView.addItemDecoration(dividerItemDecoration)
+        recyclerView.adapter = adapter
+        adapter.items = listOfFontNames
+
     }
+
+    private fun addValue() {
+        val item = TextItem("bold", "Lorem Ispum")
+        val item2 = TextItem("Italic", "Lorem Ispum")
+        val item3 = TextItem("Normal", "Lorem Ispum")
+
+
+        listOfFontNames = ArrayList<TextItem>()
+        listOfFontNames.add(item)
+        listOfFontNames.add(item2)
+        listOfFontNames.add(item3)
+    }
+
+
 }
---
+++ app/src/main/java/com/alephlab/fontstyle/model/TextItem.kt
@@ -0,0 +1,8 @@
+package com.alephlab.fontstyle.model
+
+data class TextItem(
+
+    var title: String,
+    var description: String
+
+)
